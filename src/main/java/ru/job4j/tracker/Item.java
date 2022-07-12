588d6023b95144f5c351b57b1a1bd96778168605
Artur <aaa0981@mail.ru>
2022-07-12 08:46:42 +0300

    Date. Отображение даты. [#310017 #321071]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,21 +5,21 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public LocalDateTime getCreated() {
         return created;
     }
 
-    public Item() {
-    }
-
     public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(int id, String name) {
-        this.id = id;
         this.name = name;
     }
 
