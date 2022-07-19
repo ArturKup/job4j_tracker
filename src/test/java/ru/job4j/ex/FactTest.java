f68d9ade8341cad37b7abf79a245723f08ea7725
Artur <aaa0981@mail.ru>
2022-07-19 18:49:34 +0300

    Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void WhenTheNIsLessThan0() {
+        Fact fact = new Fact();
+        fact.calc(-1);
+    }
+    @Test
+    public void When5Then120() {
+        Fact fact = new Fact();
+        int in = 5;
+        int expected = 120;
+        Assert.assertEquals(fact.calc(in), expected);
+    }
+}
+
