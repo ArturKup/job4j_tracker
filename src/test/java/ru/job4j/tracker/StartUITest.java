8aa0c253a8350fffef7d764e3e010fd4b4e4048e
Artur <aaa0981@mail.ru>
2022-07-21 10:52:24 +0300

    Тесты вывода на консоль [#33585 #324224]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -77,9 +77,9 @@ public class StartUITest {
                 new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString(), is(
-                "Menu." + System.lineSeparator()
-                       + "0. Exit" + System.lineSeparator()
+        assertThat(out.toString(), is("Menu:" + System.lineSeparator()
+                       + "0. Exit Program" + System.lineSeparator()
+                       + "=== Exit Program ===" + System.lineSeparator()
         ));
     }
 
