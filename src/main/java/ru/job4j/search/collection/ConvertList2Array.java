8a217799ea85386e96046335528ee71b1996d15b
Artur <aaa0981@mail.ru>
2022-07-29 12:52:39 +0300

    Конвертация ArrayList в двухмерный массив [#10035 #326872]

--- src/main/java/ru/job4j/search/collection/ConvertList2Array.java
+++ src/main/java/ru/job4j/search/collection/ConvertList2Array.java
@@ -1,4 +1,4 @@
-package ru.job4j.collection;
+package ru.job4j.search.collection;
 
 import java.util.List;
 
--- src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.search.collection.ConvertList2Array;
 
 import java.util.Arrays;
 
